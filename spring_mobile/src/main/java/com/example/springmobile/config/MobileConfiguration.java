3de3caacd15d0bf6d5719b188892612ec989dc02
once <itzhangnima@163.com>
2020-09-26 01:45:14 +0800

    sitePreference test

--- spring_mobile/src/main/java/com/example/springmobile/config/MobileConfiguration.java
+++ spring_mobile/src/main/java/com/example/springmobile/config/MobileConfiguration.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mobile.device.DeviceHandlerMethodArgumentResolver;
 import org.springframework.mobile.device.DeviceResolverHandlerInterceptor;
+import org.springframework.mobile.device.site.SitePreferenceHandlerInterceptor;
+import org.springframework.mobile.device.site.SitePreferenceHandlerMethodArgumentResolver;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
@@ -17,46 +19,38 @@ import java.util.List;
  *
  */
 @Configuration
-public class DeviceConfiguration extends WebMvcConfigurationSupport {
-
-//    @Bean
-//    public DeviceResolverHandlerInterceptor
-//    deviceResolverHandlerInterceptor() {
-//        return new DeviceResolverHandlerInterceptor();
-//    }
-//
-//    @Bean
-//    public SitePreferenceHandlerInterceptor
-//    sitePreferenceHandlerInterceptor() {
-//        return new SitePreferenceHandlerInterceptor();
-//    }
-//
-//    @Bean
-//    public SitePreferenceHandlerMethodArgumentResolver
-//    sitePreferenceHandlerMethodArgumentResolver() {
-//        return new SitePreferenceHandlerMethodArgumentResolver();
-//    }
+public class MobileConfiguration extends WebMvcConfigurationSupport {
 
     @Bean
-    public DeviceResolverHandlerInterceptor
-    deviceResolverHandlerInterceptor() {
+    public DeviceResolverHandlerInterceptor deviceResolverHandlerInterceptor() {
         return new DeviceResolverHandlerInterceptor();
     }
 
     @Bean
-    public DeviceHandlerMethodArgumentResolver
-    deviceHandlerMethodArgumentResolver() {
+    public DeviceHandlerMethodArgumentResolver deviceHandlerMethodArgumentResolver() {
         return new DeviceHandlerMethodArgumentResolver();
+    }
+
+    @Bean
+    public SitePreferenceHandlerInterceptor sitePreferenceHandlerInterceptor() {
+        return new SitePreferenceHandlerInterceptor();
+    }
+
+    @Bean
+    public SitePreferenceHandlerMethodArgumentResolver sitePreferenceWebArgumentResolver() {
+        return new SitePreferenceHandlerMethodArgumentResolver();
     }
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(deviceResolverHandlerInterceptor());
+        registry.addInterceptor(sitePreferenceHandlerInterceptor());
     }
 
     @Override
     public void addArgumentResolvers(
             List<HandlerMethodArgumentResolver> argumentResolvers) {
         argumentResolvers.add(deviceHandlerMethodArgumentResolver());
+        argumentResolvers.add(sitePreferenceWebArgumentResolver());
     }
 }
--- spring_mobile/src/main/java/com/example/springmobile/controller/LoginController.java
+++ spring_mobile/src/main/java/com/example/springmobile/controller/LoginController.java
@@ -1,6 +1,7 @@
 package com.example.springmobile.controller;
 
 import org.springframework.mobile.device.Device;
+import org.springframework.mobile.device.site.SitePreference;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,13 +14,25 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class LoginController {
 
-    @GetMapping("/get")
-    public String get(Device device) {
+    @GetMapping("/device")
+    public String device(Device device) {
         if (device.isMobile()) {
             return "hello mobile";
         } else if (device.isTablet()) {
             return "hello tablet";
         } else if (device.isNormal()){
+            return "hello normal";
+        }
+        return "hello unknown";
+    }
+
+    @GetMapping("/preference")
+    public String preference(SitePreference sitePreference) {
+        if (sitePreference.isMobile()) {
+            return "hello mobile";
+        } else if (sitePreference.isTablet()) {
+            return "hello tablet";
+        } else if (sitePreference.isNormal()) {
             return "hello normal";
         }
         return "hello unknown";
